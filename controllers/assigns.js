227f58177cd0157d994f4f2014f6ff7b10e08e68
Anderson Herminio <99924113+AndersonHerminio@users.noreply.github.com>
2022-09-28 17:31:44 -0300

    listar solicitações e excluir

--- controllers/assigns.js
+++ controllers/assigns.js
@@ -1,42 +1,49 @@
-myApp.controller("assignsCtrl", ["$scope", "AssignService", '$state', 'AlertMessage', function ($scope, AssignService, $state, AlertMessage) {
-    $scope.assigns = [];
+myApp.controller("assignsCtrl", ['$scope', 'AssignService', '$state', 'BookService', 'StudentService', 'AlertMessage',function($scope, AssignService, $state, BookService, StudentService, AlertMessage) {
 
-    const init = () => {
-      listAssigns();
+  $scope.books = [];
+  $scope.students = [];
+
+  const init = () => {
+    StudentService.list().then(response => {
+        $scope.students = response.data;
+    });
+
+    AssignService.list().then(response => {
+      $scope.assigns = response.data;
+    }).catch(error => console.log(error));
+  };
+
+  const deleteAssign = async (book_id, student_id) => {
+    const result = await Swal.fire({
+      title: "Deseja remover a solicitação?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Deletar",
+      cancelButtonText: "Cancelar!",
+      reverseButtons: true,
+    });
+    if (result.isConfirmed) {
+      AlertMessage.success('A solicitação foi removida.')
+      $state.reload();
+    }
+      
+    if (!result.isConfirmed) {
+      return;
+    }
+
+    const filter = {
+      book_id,
+      student_id
     };
 
-    const listAssigns = () => {
-      AssignService.list().then((response) => {
-        $scope.assigns = response.data;
-      });
-    };
+    AssignService.destroy(filter).then(() => {
+      // alert('Autor removido');
+      $state.reload();
+    }).catch(() => {
+      AlertMessage.error('Erro ao remover a solicitação!')
+    })
+  };
 
-    const deleteAssign = async (id) => {
-      const result = await Swal.fire({
-        title: "Deseja remover a solicitação?",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonText: "Deletar",
-        cancelButtonText: "Cancelar!",
-        reverseButtons: true,
-      });
-      if (result.isConfirmed) {
-        AlertMessage.success('A solicitação foi removida.')
-      }
-
-      if (!result.isConfirmed) {
-        return;
-      }
-
-      AssignService.destroy(id).then(() => {
-        // alert('Solicitação removida');
-        $state.reload();
-      }).catch(() => {
-        AlertMessage.error('Erro ao remover solicitação!')
-      })
-    };
-
-    init();
-    $scope.deleteAssign = deleteAssign;
-  },
-]);
+  init();
+  $scope.deleteAssign = deleteAssign;
+}]);
--- services/assign.js
+++ services/assign.js
@@ -8,16 +8,16 @@ myApp.service("AssignService", function ($http) {
   };
 
   const list = () => {
-    return $http.get(`${apiBaseUrl}/students/:student_id/assign-book`);
+    return $http.get(`${apiBaseUrl}/student-assigns`);
   };
 
   const find = (id) => {
     return $http.get(`${apiBaseUrl}/students/:student_id/assign-book/${id}`);
   };
 
-  const destroy = (id) => {
-    return $http.post(`${apiBaseUrl}/students/:student_id/assign-book/remove`, {
-      id,
+  const destroy = ({ book_id, student_id }) => {
+    return $http.post(`${apiBaseUrl}/student-assigns/${student_id}/return-book`, {
+      book_id: [book_id]
     });
   };
 
