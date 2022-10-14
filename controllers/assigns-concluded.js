1235d847ea9438d65d3a1989a3a6aaba5fc6e975
Anderson Herminio <99924113+AndersonHerminio@users.noreply.github.com>
2022-10-14 18:16:29 -0300

    assign-concluded atualizado

--- controllers/assigns-concluded.js
+++ controllers/assigns-concluded.js
@@ -1,16 +1,9 @@
-myApp.controller("assignsConcludedCtrl", ['$scope', 'AssignService', '$state', 'BookService', 'StudentService', 'AlertMessage',function($scope, AssignService, $state, BookService, StudentService, AlertMessage) {
-
-  $scope.books = [];
-  $scope.students = [];
-
+myApp.controller("assignsConcludedCtrl", ['$scope', 'AssignService', function ($scope, AssignService) {
   const init = () => {
-    StudentService.find().then(response => {
-        $scope.students = response.data;
-    });
-
     AssignService.find().then(response => {
       $scope.assigns = response.data;
     }).catch(error => console.log(error));
   };
+
   init();
 }]);
